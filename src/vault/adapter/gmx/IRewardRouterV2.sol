ccd3b697e91904564100367332c17adc1951dcea
aritkulova <aritkulova.yuliia@gmail.com>
2023-08-04 18:28:39 +0300

    added glp adapter, draft

--- src/vault/adapter/gmx/IRewardRouterV2.sol
+++ src/vault/adapter/gmx/IRewardRouterV2.sol
@@ -3,6 +3,10 @@
 pragma solidity ^0.8.15;
 
 interface IRewardRouterV2 {
+    function glp() external view returns(address);
+    function mintAndStakeGlp(address _token, uint256 _amount, uint256 _minUsdg, uint256 _minGlp) external returns (uint256);
+    function unstakeAndRedeemGlp(address _tokenOut, uint256 _glpAmount, uint256 _minOut, address _receiver) external returns (uint256);
+
     function feeGlpTracker() external view returns (address);
     function stakedGlpTracker() external view returns (address);
 
